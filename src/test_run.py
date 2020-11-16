16bfcadb10da1e2745c6687179cbe5d1d084b85a
madsemildahlgaard <madsemildh@gmail.com>
2020-11-16 14:51:43 +0100

    Add done check in  evaluation

--- src/test_run.py
+++ src/test_run.py
@@ -180,19 +180,30 @@ torch.save(policy.state_dict, 'checkpoint.pt')
 eval_env = make_env(num_envs, start_level=num_levels, num_levels=num_levels)
 obs = eval_env.reset()
 
+frames = []
 total_reward = []
 
 # Evaluate policy
 policy.eval()
-for _ in range(512):
+# Play 10 times
+for _ in range(10):
+    done = False
+    while not done:
+        # Use policy
+        action, log_prob, value = policy.act(obs)
 
-    # Use policy
-    action, log_prob, value = policy.act(obs)
+        # Take step in environment
+        obs, reward, done, info = eval_env.step(action)
+        total_reward.append(torch.Tensor(reward))
 
-    # Take step in environment
-    obs, reward, done, info = eval_env.step(action)
-    total_reward.append(torch.Tensor(reward))
+        # Render environment and store
+        frame = (torch.Tensor(eval_env.render(mode='rgb_array'))*255.).byte()
+        frames.append(frame)
 
 # Calculate average return
 total_reward = torch.stack(total_reward).sum(0).mean(0)
 print('Average return:', total_reward)
+
+# Save frames as video
+frames = torch.stack(frames)
+imageio.mimsave('vid1.mp4', frames, fps=25)
