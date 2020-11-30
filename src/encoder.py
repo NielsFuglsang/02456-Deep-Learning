b64717384c2843f7fe5a73533797f37e31549705
Sleinomagic <s164181@student.dtu.dk>
2020-11-30 14:08:57 +0100

    Impala block structure

--- src/encoder.py
+++ src/encoder.py
@@ -26,14 +26,40 @@ class Impala(nn.Module):
     def __init__(self, in_channels, feature_dim):
         super().__init__()
         self.layers = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=16, kernel_size=5, stride=2), nn.ReLU(), nn.BatchNorm2d(16), nn.MaxPool2d(2),
-            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1), nn.ReLU(), nn.BatchNorm2d(32), nn.MaxPool2d(2),
-            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1), nn.ReLU(), nn.BatchNorm2d(32),
+            ImpalaBlock(in_channels=in_channels, out_channels=16),
+            ImpalaBlock(in_channels=16, out_channels=32),
+            ImpalaBlock(in_channels=32, out_channels=32), nn.ReLU(),
             Flatten(),
-            nn.Linear(in_features=288, out_features=feature_dim), nn.ReLU()
+            nn.Linear(in_features=32 * 8 * 8, out_features=feature_dim), nn.ReLU()
         )
         self.apply(orthogonal_init)
 
     def forward(self, x):
         x = self.layers(x)
         return x
+
+class ImpalaBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ImpalaBlock, self).__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            ResidualBlock(out_channels),
+            ResidualBlock(out_channels),
+        )
+
+    def forward(self, x):
+        x = self.block(x)
+        return x
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.ReLU(), nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(), nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1, padding=1)
+        )
+
+    def forward(self, x):
+        out = self.layers(x)
+        return out + x
